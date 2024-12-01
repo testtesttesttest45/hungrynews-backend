f5deb642a45325c42f621b4d93e1eb0cfcfe2794
testtesttesttest45 <weilinquek201@gmail.com>
2024-12-01 13:14:30 +0800

    TYPO!!!

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def assetlinks():
         {
             "relation": ["delegate_permission/common.handle_all_urls"],
             "target": {
-                "namespace": "hungry_news",
+                "namespace": "android_app",
                 "package_name": "com.example.hungry_news",
                 "sha256_cert_fingerprints": [
                     "55:49:E1:D9:DF:E7:17:20:81:F8:6B:E1:05:9D:67:B2:EA:2F:08:B2:05:04:12:02:5F:EC:E8:AF:1D:D6:66:79"
